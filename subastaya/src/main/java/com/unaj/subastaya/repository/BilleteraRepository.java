f6f69afbbc36fcba1296c6fcb15a7e59f8751321
Facundo Santillan <facundoosantillan021@gmail.com>
2026-08-30 21:02:53 -0300

    feat: repositorio de billetera

---
+++ subastaya/src/main/java/com/unaj/subastaya/repository/BilleteraRepository.java
@@ -0,0 +1,11 @@
+package com.unaj.subastaya.repository;
+
+import com.unaj.subastaya.model.Billetera;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BilleteraRepository extends JpaRepository<Billetera, Long> {
+
+    Optional<Billetera> findByUsuarioId(Long usuarioId);
+}
