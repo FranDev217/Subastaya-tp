50244384c3a10a0c27b45e2b9de71dd06d29ac11
Facundo Santillan <facundoosantillan021@gmail.com>
2026-08-30 21:16:51 -0300

    feat: DTO Deposito

---
+++ subastaya/src/main/java/com/unaj/subastaya/dto/DepositoRequest.java
@@ -0,0 +1,11 @@
+package com.unaj.subastaya.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record DepositoRequest(
+        @NotNull @Positive BigDecimal monto
+) {
+}
