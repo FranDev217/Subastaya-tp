e0a64cd1952b5637b10e9f10950302b327410673
Facundo Santillan <facundoosantillan021@gmail.com>
2026-09-08 21:28:30 -0300

    feat: agregamos excepcion de credenciales invalidas

---
+++ backend/src/main/java/com/unaj/subastaya/exception/CredencialesInvalidasException.java
@@ -0,0 +1,8 @@
+package com.unaj.subastaya.exception;
+
+public class CredencialesInvalidasException extends RuntimeException {
+
+    public CredencialesInvalidasException() {
+        super("Email o contraseña incorrectos");
+    }
+}
