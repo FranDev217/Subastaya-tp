7a978675df7569371e153310c27d81335a873927
Franco Destefani <f.destefani12@gmail.com>
2026-09-11 17:31:32 -0300

    feat: configurar rutas principales con React Router
    - Agrega react-router-dom v7.18.3 a dependencias.
    - Configura BrowserRouter y Routes en App.jsx para / (CatalogoPage), /login (LoginPage) y redirección por defecto (*).

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import CatalogoPage from './pages/CatalogoPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
-  return <CatalogoPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CatalogoPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
