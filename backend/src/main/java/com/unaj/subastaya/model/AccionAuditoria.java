7ae4ab9b9873b22c6513da197856a3a321b9ac32
Franco Destefani <f.destefani12@gmail.com>
2026-09-11 10:38:16 -0300

    feat: agregar constante APERTURA_WORKER en AccionAuditoria

--- backend/src/main/java/com/unaj/subastaya/model/AccionAuditoria.java
+++ backend/src/main/java/com/unaj/subastaya/model/AccionAuditoria.java
@@ -5,6 +5,7 @@ public final class AccionAuditoria {
     public static final String PUJA_RECHAZADA = "PUJA_RECHAZADA";
     public static final String EXTENSION_TIEMPO = "EXTENSION_TIEMPO";
     public static final String CIERRE_WORKER = "CIERRE_WORKER";
+    public static final String APERTURA_WORKER = "APERTURA_WORKER";
     public static final String ACREDITACION_MANUAL = "ACREDITACION_MANUAL";
     public static final String SUBASTA_CREADA = "SUBASTA_CREADA";
 
